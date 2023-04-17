96fe347af5df1387eb03dfaf3ebd7e5d856394fe
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2023-04-17 12:14:02 -0600

    fix: se agrega configuracion en next para que busque los archivos .wasm y .proto de tesseract

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.proto'],
+    },
+  },
 }
 
 module.exports = nextConfig
